337f37384070268a7f04c0e70bf52b77837df981
Caleb Rash <caleb@brilliant.org>
2017-03-09 21:22:21 -0600

    Move JS, rebuild

--- static/dist/app.js
+++ static/dist/app.js
@@ -1,21 +1,29 @@
 'use strict';
 
 function getMeetups(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
         xhr.open('GET', url, true);
         xhr.send();
 
-        xhr.addEventListener('load', () => resolve(xhr.responseText));
-        xhr.addEventListener('error', () => reject(xhr.responseText));
-        xhr.addEventListener('abort', () => reject(xhr.responseText));
+        xhr.addEventListener('load', function () {
+            return resolve(xhr.responseText);
+        });
+        xhr.addEventListener('error', function () {
+            return reject(xhr.responseText);
+        });
+        xhr.addEventListener('abort', function () {
+            return reject(xhr.responseText);
+        });
     });
 }
 
 window.addEventListener('DOMContentLoaded', function () {
-    const meetupsContainer = document.getElementById('meetups-list');
+    var meetupsContainer = document.getElementById('meetups-list');
     if (meetupsContainer) {
-        getMeetups(meetupsContainer.dataset.meetupsUrl)
-            .then((result) => meetupsContainer.innerHTML = result);
+        getMeetups(meetupsContainer.dataset.meetupsUrl).then(function (result) {
+            return meetupsContainer.innerHTML = result;
+        });
     }
 });
+
--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
-    // js goes here
+function getMeetups(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.send();
+
+        xhr.addEventListener('load', () => resolve(xhr.responseText));
+        xhr.addEventListener('error', () => reject(xhr.responseText));
+        xhr.addEventListener('abort', () => reject(xhr.responseText));
+    });
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+    const meetupsContainer = document.getElementById('meetups-list');
+    if (meetupsContainer) {
+        getMeetups(meetupsContainer.dataset.meetupsUrl)
+            .then((result) => meetupsContainer.innerHTML = result);
+    }
 });
